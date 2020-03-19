d3219d5bfd7199269a91fbe23f44bcfca52ae27d
aamirb123 <61799947+aamirb123@users.noreply.github.com>
2020-03-20 00:05:16 +0530

    Add files via upload

---
+++ Nullable8.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nullabletype
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool? AreYouMajor = null;
+            if (AreYouMajor == true)
+            {
+                Console.WriteLine("User is Major");
+            }
+            else if (!AreYouMajor.Value)
+            {
+                Console.WriteLine("User is not Major");
+            }
+
+            else
+            {
+                Console.WriteLine("User is not answer your question");
+            }
+        }
+    }
+}
