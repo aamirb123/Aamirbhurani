15487b7265384d353f818d2e18522b0e7d040610
aamirb123 <61799947+aamirb123@users.noreply.github.com>
2020-03-20 00:42:35 +0530

    Add files via upload

---
+++ Arrays10.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Arrays10
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] EvenNum = new int[3];
+            EvenNum[0] = 0;
+            EvenNum[1] = 2;
+            EvenNum[2] = 4;
+            Console.WriteLine(EvenNum[1]);
+        }
+    }
+}
