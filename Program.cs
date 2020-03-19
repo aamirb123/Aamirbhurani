e56a8004941401c39eb3da174afa105d2196c539
aamirb123 <61799947+aamirb123@users.noreply.github.com>
2020-03-19 16:14:02 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Firstprog
+namespace prog2
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("welcome Aamir for  c# training video for mastek");
+            Console.WriteLine("please enter yor name");
+            String UserName = Console.ReadLine();
+            Console.WriteLine("Hello" +UserName);           //concatination method.
+            Console.WriteLine("Hello {0}", UserName);       //by using placeholder method.
         }
     }
 }
