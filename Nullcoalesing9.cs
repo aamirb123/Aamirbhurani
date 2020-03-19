7065163c6a027901f94999e6997dedaa9321cc97
aamirb123 <61799947+aamirb123@users.noreply.github.com>
2020-03-20 00:25:30 +0530

    Add files via upload

---
+++ Nullcoalesing9.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nullcoalesing9
+{
+    class Program
+    {
+        static void Main()
+        {
+            int? TicketonSale = null;
+            int AvailableTickets = TicketonSale ?? 0;
+            Console.WriteLine("Available Tickets ={0}", AvailableTickets);
+        }
+    }
+}
