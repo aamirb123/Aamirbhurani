24d253090f90b0736a600eaee657197464e238ef
aamirb123 <61799947+aamirb123@users.noreply.github.com>
2020-03-19 17:02:15 +0530

    Add files via upload

---
+++ string code 4.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace code4
+{
+    class Program
+    {
+        static void Main()
+        {
+            String Name = "Aamir Bhurani";
+            Console.WriteLine(Name);
+        }
+    }
+}
