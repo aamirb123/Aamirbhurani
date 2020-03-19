ff95e3f1eaf598b87810b7f6fcf636709519203a
aamirb123 <61799947+aamirb123@users.noreply.github.com>
2020-03-19 17:24:33 +0530

    Add files via upload

---
+++ operator 5 code.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace operators_5
+{
+    class Program
+    {
+        static void Main()
+        {
+            int numerator = 10;
+            int denominator = 5;
+            int result = numerator / denominator;   // it divide the result and give them quietient
+            int result1 = numerator % denominator;  // it gives us reminder
+
+            Console.WriteLine("result {0}", result);
+            Console.WriteLine("result1 {0}", result1);
+
+
+        }
+    }
+}
