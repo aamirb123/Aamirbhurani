345a3bfbfc78540ca280b83e1ea6e222f3e955b7
aamirb123 <61799947+aamirb123@users.noreply.github.com>
2020-03-19 23:32:31 +0530

    Add files via upload

---
+++ ternary operator6.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ternary_operator
+{
+    class Program
+    {
+        static void Main()
+        {
+            int Num = 13;
+            bool IsNum;
+
+            bool IsNum1 = Num == 10 ? true : false;  // ternary operator replace by ifelse statements so itvreduces a block of code.
+            Console.WriteLine(" Num==10 is{0}", IsNum1);
+        }
+    }
+}
