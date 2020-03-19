309ef89b94fc1869e4eaa91fdc4bf0c7bdc18675
aamirb123 <61799947+aamirb123@users.noreply.github.com>
2020-03-19 15:31:16 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Firstprog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome Aamir for  c# training video for mastek");
+        }
+    }
+}
