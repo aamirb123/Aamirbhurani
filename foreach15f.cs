337a26964434db841be8adfc3f015d3aa21fd5f6
aamirb123 <61799947+aamirb123@users.noreply.github.com>
2020-03-20 16:57:13 +0530

    Add files via upload

---
+++ foreach15f.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace foreach15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Number=new  int[3];
+            Number[0] = 101;
+            Number[1] = 102;
+            Number[2] = 103;
+            foreach (int i in Number)
+            {
+                Console.WriteLine(i);
+            }
+        
+        }
+    }
+}
