aeadbad9feb2ef05b9387ae8ba3687273f509f54
aamirb123 <61799947+aamirb123@users.noreply.github.com>
2020-03-20 15:43:05 +0530

    Add files via upload

---
+++ Forloop14.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace forloop14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Number = new int[3];
+            Number[0] = 101;
+            Number[1] = 102;
+            Number[2] = 103;
+            for (int i = 0; i <= Number.Length; i++)
+            {
+                Console.WriteLine(Number[i]);
+            }
+        }
+    }
+}
