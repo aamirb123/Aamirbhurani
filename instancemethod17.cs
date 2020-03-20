ff7e0849e1eaf23ed62d367c826d25478641c97b
aamirb123 <61799947+aamirb123@users.noreply.github.com>
2020-03-20 17:51:19 +0530

    Add files via upload

---
+++ instancemethod17.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace instancemethod17
+{
+    class Program
+    {
+        static void Main()
+        {
+            Program.EvenNumber(30);
+
+            Program p = new Program();
+            int sum = p.Add(10, 20);
+            Console.WriteLine("sum = {0}", sum);
+        }
+
+        public int Add(int FN, int SN)
+        {
+            return FN + SN;
+        }
+
+        public static void EvenNumber(int Target)
+        {
+            int start = 0;
+            while (start <= Target)
+            {
+                Console.WriteLine(start);
+                start = start + 2;
+            }
+
+        }
+    }
+}
+
