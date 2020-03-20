5839b2578286e2d627e27a5d5d4864e9097e567a
aamirb123 <61799947+aamirb123@users.noreply.github.com>
2020-03-20 15:12:43 +0530

    Add files via upload

---
+++ 13whilecode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _13whileloop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter target");
+            int UserName = int.Parse(Console.ReadLine());
+            int start = 0;
+            while (start <= UserName)
+            {
+                Console.WriteLine(start);
+                start = start + 2;
+            }
+        }
+    }
+}
