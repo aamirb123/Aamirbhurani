3331f5c540fda11a64ccf214b8c7a024a50d1cf2
aamirb123 <61799947+aamirb123@users.noreply.github.com>
2020-03-20 17:21:31 +0530

    Add files via upload

---
+++ instancemethod.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Methods16
+{
+    class Program
+    {
+        static void Main()
+        {
+            Program p = new Program();  //instance method format
+            p.EvenNumber();
+
+        }
+        public void EvenNumber()     ///public method declaration
+        {
+            int start = 0;
+            while (start <= 20)
+            {
+                Console.WriteLine(start);
+                start = start + 2;
+            }
+        }
+    }
+}
