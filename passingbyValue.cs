a1c4e2affd2ff6950fb96a2a17c4b7a14e1d6c4c
aamirb123 <61799947+aamirb123@users.noreply.github.com>
2020-03-21 12:52:57 +0530

    Add files via upload

---
+++ passingbyValue.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace datatypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int i = 0;
+            simpleMethod(i);
+
+            Console.WriteLine(i);
+        }
+        public static void simpleMethod(int j)
+        {
+            j = 101;
+        }
+
+    }
+}
