4def6dcc3e2e384e5d3c5d0f36eb66343c3b6240
aamirb123 <61799947+aamirb123@users.noreply.github.com>
2020-03-21 13:27:38 +0530

    Add files via upload

---
+++ integerparameters.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace datatypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int[] Numbers = new int[3];
+
+            Numbers[0] = 101;
+            Numbers[1] = 102;
+            Numbers[2] = 103;
+             parasMethod(Numbers);
+//parasMethod();
+ 
+           
+        }
+        public static void parasMethod( params int[] Numbers )
+        {
+            Console.WriteLine(" there are {0}   elements", Numbers.Length);
+            foreach (int i in Numbers)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+    }
+}
