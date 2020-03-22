559e83c6cc95388ca39730bdb1cea00a33dd209f
aamirb123 <61799947+aamirb123@users.noreply.github.com>
2020-03-22 20:29:25 +0530

    Add files via upload

---
+++ Practice.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myNum = 5;
+            float value = 10.3F;
+            string Name = "Aamir Bhurani";
+            bool Myname = true;
+            char letters = 'A';
+            Console.WriteLine(myNum);
+            Console.WriteLine(value);
+            Console.WriteLine("Hello " + Name);  // concatination or combinig of two elements 
+            Console.WriteLine(Myname);
+            Console.WriteLine(letters);
+        }
+    }
+}
